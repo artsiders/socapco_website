ddc28a3b4e77c12283ff96c5ad27b7e0e3958214
salim <jimsky699@gmail.com>
2022-07-30 11:49:46 +0100

    add register form

--- views/register_form.php
+++ views/register_form.php
@@ -1,1 +1,38 @@
-<h1>register form</h1>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+	<title>Socapco - register</title>
+	<link rel="stylesheet" href="<?= ASSETS ?>css/form.css">
+</head>
+
+<body>
+	<div class="container">
+		<div class="content">
+			<div class="image-box">
+				<img src="<?= ASSETS ?>images/contact.png" alt="">
+			</div>
+			<form action="#">
+				<div class="topic">Send us a message</div>
+				<div class="input-box">
+					<input type="text" required>
+					<label>Enter your name</label>
+				</div>
+				<div class="input-box">
+					<input type="text" required>
+					<label>Enter your email</label>
+				</div>
+				<div class="message-box">
+					<textarea></textarea>
+					<label>Enter your message</label>
+				</div>
+				<div class="input-box">
+					<input type="submit" value="Send Message">
+				</div>
+			</form>
+		</div>
+	</div>
+
+</body>
+
+</html>
