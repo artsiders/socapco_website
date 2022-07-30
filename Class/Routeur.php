311571d37b6bede2f03983e2dfe0f37af2ce7980
salim <jimsky699@gmail.com>
2022-07-30 08:49:48 +0100

    make admin route

--- Class/Routeur.php
+++ Class/Routeur.php
@@ -8,7 +8,6 @@ class Routeur
     }
     public function renderController()
     {
-
         if ($this->request == "home" || $this->request == "") {
             include VIEW . "home.php";
         }
@@ -20,6 +19,29 @@ class Routeur
         }
         if ($this->request == "contact") {
             include VIEW . "contact.php";
+        }
+
+        // route admin app
+        if ($this->request == "admin") {
+            if (isset($_SESSION['socapco_admin']) and !empty($_SESSION['socapco_admin'])) {
+                include VIEW . "dashboard.php";
+            } else {
+                include VIEW . "login_form.php";
+            }
+        }
+        if ($this->request == "login") {
+            if (isset($_SESSION['socapco_admin']) and !empty($_SESSION['socapco_admin'])) {
+                header("location: admin");
+            } else {
+                include VIEW . "login_form.php";
+            }
+        }
+        if ($this->request == "register") {
+            if (isset($_SESSION['socapco_admin']) and !empty($_SESSION['socapco_admin'])) {
+                header("location: admin");
+            } else {
+                include VIEW . "register_form.php";
+            }
         }
     }
 }
