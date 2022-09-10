229759d93d36160a8b9d3bdbbe53437748871136
salim <jimsky699@gmail.com>
2022-09-10 07:14:57 +0100

    clean _gallery

--- views/partials/_galery.php
+++ views/partials/_galery.php
@@ -25,27 +25,6 @@
         <?php endforeach; ?>
     </section>
 
-    <?php foreach ($allProducts as $key => $product) : ?>
-        <div class="wow d-none fadeInUp card_product_main filter-<?= $effect["id_effect"] ?>" data-wow-delay="400ms">
-            <div class="image_box">
-                <img src="<?= ASSETS ?>images/product/<?= $product["picture"] ?>" alt="">
-            </div>
-            <div class="body">
-                <h5><?= $product["name"] ?></h5>
-                <span>By Simon Smith | March 18, 2018</span>
-                <div class="">
-                    <i class="fa fa-star" aria-hidden="true"></i>
-                    <i class="fa fa-star" aria-hidden="true"></i>
-                    <i class="fa fa-star" aria-hidden="true"></i>
-                    <i class="fa fa-star" aria-hidden="true"></i>
-                    <i class="fa fa-star-o" aria-hidden="true"></i>
-                </div>
-                <p><?= $product["description"] ?></p>
-                <a href="./product_detail&id=<?= $product["id_product"] ?>" class="btn academy-btn btn-sm">voir
-                    plus</a>
-            </div>
-        </div>
-    <?php endforeach; ?>
     <!-- partial -->
     <script src='<?= ASSETS ?>libs/galery/jquery.min.js'></script>
     <script src='<?= ASSETS ?>libs/galery/masonry.min.min.js'>
