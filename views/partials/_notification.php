f805c249ec3120930d1d01362d0033657f6f7ac8
salim <jimsky699@gmail.com>
2022-09-13 14:16:35 +0100

    make fix notification

--- views/partials/_notification.php
+++ views/partials/_notification.php
@@ -4,12 +4,38 @@ if (isset($_SESSION['socapco_alert']) && !empty($_SESSION['socapco_alert']))
 else $alert = array();
 
 ?>
+
+<style>
+    @keyframes notification {
+        0% {
+            opacity: 1;
+            transform: translateY(0);
+        }
+
+        100% {
+            opacity: 0;
+            bottom: -200px;
+            pointer-events: none;
+        }
+    }
+
+    .notification_box {
+        position: fixed;
+        z-index: 2;
+        right: 0;
+        bottom: 5px;
+        width: 300px;
+        margin: 20px;
+        animation: notification 1s linear 8s forwards;
+        box-shadow: 0 0 10px #878787;
+    }
+</style>
 <?php if (!empty($alert)) : ?>
     <?php
     if ($alert['type'] == "error") $color = "danger";
     else $color = $alert['type'];
     ?>
-    <div class="alert alert-<?= $color ?>">
+    <div class="notification_box alert alert-<?= $color ?>">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button>
         <h3 class="text-<?= $alert['type'] ?>">
             <?php if ($alert['type'] == "success") echo "<i class='fa fa-check-circle'></i> succès"; ?>
