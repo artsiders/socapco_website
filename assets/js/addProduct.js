e438799c815af272d8669e26e1f0cc96167c79ca
salim <jimsky699@gmail.com>
2022-09-21 09:23:20 +0100

    add image conpressor on addproduct

--- assets/js/addProduct.js
+++ assets/js/addProduct.js
@@ -1,4 +1,5 @@
 const addProductForm = document.getElementById("addProductForm")
+const uploadFileAdd = document.getElementById("uploadFileAdd")
 
 let errorBox = document.getElementById("addError")
 let successBox = document.getElementById("addSuccess")
@@ -8,54 +9,77 @@ let imageLabel = document.getElementById("imageLabel")
 addProductForm.addEventListener("submit", e => {
     e.preventDefault()
 
-    let data = new FormData(addProductForm)
+    let file = uploadFileAdd.files[0]
+    if (!file) {
+        errorBox.classList.remove("hide")
+        errorBox.innerHTML = "selectionner une image pour le produit"
+        return;
+    } else {
+        let type = file.type;
 
-    fetch(`${location.origin}/socapco_website/controlers/addProduct.php`, {
-        method: 'POST',
-        headers: {
-            'X-Requested-With': 'xmlhttprequest'
-        },
-        body: data
-    }).then(responce => responce.json()).then(data => {
-        if (!data.name) {
-            addProductForm.name.classList.add("border-danger")
-        }
-        else {
-            addProductForm.name.classList.remove("border-danger")
-        }
-        if (!data.description) {
-            addProductForm.description.classList.add("border-danger")
-        }
-        else {
-            addProductForm.description.classList.remove("border-danger")
-        }
-        if (!data.image) {
-            imageLabel.classList.add("border-danger")
-        }
-        else {
-            imageLabel.classList.remove("border-danger")
-        }
-
-        if (data.msg == "") {
-            if (data.isOk) {
-                window.location.reload()
-                // errorBox.classList.add("hide")
-                // successBox.classList.remove("hide")
-                // successBox.innerHTML = `produit ajouter avec succé ! 
-                // <button type="button" class="close" data-dismiss="modal"
-                // aria-hidden="true">×</button>`;
-
-                // addProductForm.name.value = ""
-                // addProductForm.description.value = ""
-                // addProductForm.image.value = ""
-            } else {
-                errorBox.classList.remove("hide")
-                errorBox.innerHTML = "ERREUR <strong>500</strong>"
-            }
-        } else {
+        if (type != "image/jpeg" && type != "image/jpg" && type != "image/png") {
             errorBox.classList.remove("hide")
-            errorBox.innerHTML = data.msg
-            successBox.classList.add("hide")
+            errorBox.innerHTML = "le ficher selectionner n'est pas une image"
+            return;
+        } else {
+            errorBox.classList.add("hide")
         }
-    })
-})
+    }
+
+    new Compressor(file, {
+        quality: 0.2,
+
+        // The compression process is asynchronous,
+        // which means you have to access the `result` in the `success` hook function.
+        success(result) {
+            const formData = new FormData(addProductForm);
+
+            // The third parameter is required for server
+            formData.append('product_image', result, result.name);
+
+            fetch(`${location.origin}/socapco_website/controlers/addProduct.php`, {
+                method: 'POST',
+                headers: {
+                    'X-Requested-With': 'xmlhttprequest'
+                },
+                body: formData
+            }).then(responce => responce.json()).then(data => {
+                if (!data.name) {
+                    addProductForm.name.classList.add("border-danger")
+                }
+                else {
+                    addProductForm.name.classList.remove("border-danger")
+                }
+                if (!data.description) {
+                    addProductForm.description.classList.add("border-danger")
+                }
+                else {
+                    addProductForm.description.classList.remove("border-danger")
+                }
+                if (!data.image) {
+                    imageLabel.classList.add("border-danger")
+                }
+                else {
+                    imageLabel.classList.remove("border-danger")
+                }
+
+                if (data.msg == "") {
+                    if (data.isOk) {
+                        window.location.reload()
+                    } else {
+                        errorBox.classList.remove("hide")
+                        errorBox.innerHTML = "ERREUR <strong>500</strong>"
+                    }
+                } else {
+                    errorBox.classList.remove("hide")
+                    errorBox.innerHTML = data.msg
+                    successBox.classList.add("hide")
+                }
+            })
+        },
+        error(err) {
+            console.log(err.message);
+        },
+    });
+
+});
--- views/partials/admin/_list_products.php
+++ views/partials/admin/_list_products.php
@@ -32,7 +32,7 @@
 													<div class="col-1">
 														<label style="height: 35px;" for="uploadFileEdit" id="imageLabel" class="fileupload btn btn-outline-secondary waves-effect waves-light">
 															<span><i class="fas fa-image"></i></span>
-															<input name="image" type="file" id="uploadFileEdit" class="upload" hidden>
+															<input name="image" type="file" id="uploadFileEdit" class="upload" hidden accept="image/*">
 														</label>
 													</div>
 												</div>
@@ -127,7 +127,7 @@
 													<div class="col-1">
 														<label style="height: 35px;" for="uploadFileAdd" id="imageLabel" class="fileupload btn btn-outline-secondary waves-effect waves-light">
 															<span><i class="fas fa-image"></i></span>
-															<input name="product_image" type="file" id="uploadFileAdd" class="upload" hidden>
+															<input type="file" id="uploadFileAdd" class="upload" hidden accept="image/*">
 														</label>
 													</div>
 												</div>
@@ -298,6 +298,7 @@
 	</div>
 </div>
 
+<script src="<?= ASSETS ?>libs/compressorjs/compressor.min.js"></script>
 <script src="<?= ASSETS ?>js/addProduct.js"></script>
 <script src="<?= ASSETS ?>js/deleteProd.js"></script>
 <script src="<?= ASSETS ?>js/editProd.js"></script>
